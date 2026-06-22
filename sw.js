cfaae6dcf9b40da5f789bc5340dbf2f394a7a706
mickey1702 <hemanshukm@gmail.com>
2026-06-23 00:45:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,1 @@
-const CACHE_NAME = 'telegallery-v1';
-const urlsToCache = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
